0510985cd3b5967e1d0798dcb1704b2ef7ad37a7
Silvino <65027563+silvinoan@users.noreply.github.com>
2023-02-09 17:45:00 -0300

    Update managed_mssql_instance.tf

--- managed_mssql_instance.tf
+++ managed_mssql_instance.tf
@@ -5,34 +5,12 @@ resource "azurerm_mssql_managed_instance" "primary" {
 
   license_type       = "BasePrice"
   sku_name           = "GP_Gen5"
-  storage_size_in_gb = 32
+  storage_size_in_gb = 256
   subnet_id          = data.azurerm_subnet.this.id
   vcores             = 4
+  collation          = "SQL_Latin1_General_CP1_CI_AI"
 
   administrator_login          = "${local.sqlmi_names[0]}admin"
-  administrator_login_password = random_password.password.result
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = local.tags
-}
-
-resource "azurerm_mssql_managed_instance" "secondary" {
-  count = var.enable_failover ? 1 : 0
-
-  name                = local.sqlmi_names[1]
-  resource_group_name = data.azurerm_resource_group.this.name
-  location            = data.azurerm_resource_group.this.location
-
-  license_type       = "BasePrice"
-  sku_name           = "GP_Gen5"
-  storage_size_in_gb = 32
-  subnet_id          = data.azurerm_subnet.this.id
-  vcores             = 4
-
-  administrator_login          = "${local.sqlmi_names[1]}admin"
   administrator_login_password = random_password.password.result
 
   identity {
